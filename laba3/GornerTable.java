8d16fcb855f78652c05381dca7b8f9ce42df12c0
Yan Laworenko <4mo.takoe@mail.ru>
2019-12-28 13:32:19 +0300

    feat: add lab3 to lab2 branch

---
+++ laba3/GornerTable.java
@@ -0,0 +1,97 @@
+package bsu.rfe.java.group10.lab3.YanLavorenko.var1C;
+
+import javax.swing.table.AbstractTableModel;
+
+
+public class GornerTable extends AbstractTableModel {
+
+    private Double[] coeff;
+    private Double from;
+    private Double to;
+    private Double step;
+
+    public GornerTable(Double from, Double to, Double step, Double[] coeff)
+    {
+        this.from = from;
+        this.to = to;
+        this.step = step;
+        this.coeff = coeff;
+    }
+
+    public Double[] getCoeff() {
+        return coeff;
+    }
+
+    public Double getFrom() {
+        return from;
+    }
+
+    public Double getTo() {
+        return to;
+    }
+
+    public Double getStep() {
+        return step;
+    }
+
+    @Override
+    public int getRowCount() {
+        if(((to-from)/step)-(int)((to-from)/step)==0.0)
+            return new Double(Math.ceil((to-from)/step)).intValue()+1;
+        else
+            return new Double(Math.ceil((to-from)/step)).intValue();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 4;
+    }
+
+    @Override
+    public String getColumnName(int col) {
+        switch (col)
+        {
+            case 0:
+                return "Значение X";
+            case 1:
+                return "Значение многочлена при X+2";
+            case 2:
+                return "Значение многочлена при 2*X + 1";
+            default:
+                return "Разница между значениями";
+        }
+    }
+
+    @Override
+    public Class<?> getColumnClass(int col) {
+        return Double.class;
+    }
+
+    @Override
+    public Object getValueAt(int row, int col) {
+        Double x=from+step*row;
+        Double result1=0.0, result2=0.0;
+
+        for(int i=0;i<coeff.length-1;i++)
+        {
+            result1 = x + 2;
+        }
+        result1+=coeff[coeff.length-1];
+
+        for(int i=coeff.length-1;i>0;i--)
+        {
+            result2 = 2*x + 1;
+        }
+        result2+=coeff[0];
+
+        if(col==0)
+            return x;
+        else if(col==1)
+            return result1;
+
+        else if (col==2)
+            return result2;
+        else
+            return result2-result1;
+    }
+}
